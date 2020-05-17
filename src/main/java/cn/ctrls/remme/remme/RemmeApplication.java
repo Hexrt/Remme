d8a470335ddb00bb56cffd47f2b1ee479f236ca1
Hexrt <Hexrt@yoosec.com>
2020-05-17 11:55:54 +0800

    firsts step

---
+++ src/main/java/cn/ctrls/remme/remme/RemmeApplication.java
@@ -0,0 +1,13 @@
+package cn.ctrls.remme.remme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ctrls/remme/remme/RemmeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ctrls.remme.remme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
